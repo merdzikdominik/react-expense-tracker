ea18cf770c058d5a909d6b0b267bf337ac11c774
Dominik <dominik.merdzik@onet.pl>
2022-04-21 13:43:54 +0200

    pushing to remote

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,24 @@
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: 'numeric'});
+    const year = props.date.getFullYear();
+
+    return (
+        <div className="expense-item">
+            <div className="expense-date-container">
+                <div>{ month }</div>
+                <div>{ day }</div>
+                <div>{ year }</div>
+            </div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
