a57345fe3632fef0af16204a9e57510ec5328916
Dominik <dominik.merdzik@onet.pl>
2022-05-05 13:50:50 +0200

    fixed a bug related with converting amount from string to int

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -24,7 +24,7 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount, //+ as number conversion from string
             date: new Date(enteredDate)
         }
 
